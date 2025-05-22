eda672c6a83eaf72ecc21fd5bc652cafd2345eaa
0xRFMN <116576587+0xRFMN@users.noreply.github.com>
2025-05-22 12:51:28 -0400

    Update settings_generative.py

--- infra/settings_generative.py
+++ infra/settings_generative.py
@@ -32,7 +32,7 @@ from datarobot_pulumi_utils.schema.llms import (
 
 from utils.schema import LLMDeploymentSettings
 
-LLM = LLMs.DEPLOYED_LLM
+LLM = LLMs.AZURE_OPENAI_GPT_4_TURBO
 
 custom_model_args = CustomModelArgs(
     resource_name=f"Generative Analyst Custom Model [{PROJECT_NAME}]",
